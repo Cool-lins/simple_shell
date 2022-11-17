893aeae30007a5ab49511728f9c55444d97715d7
Cool-lins <collinseonyekwere@gmail.com>
2022-11-17 07:45:16 -0800

    updates

--- memory.c
+++ memory.c
@@ -1,26 +1,62 @@
-#include "simpleshell.h"
+#include "main.h"
 
 /**
- *_calloc -allocated memoria for nmeb elemn de zise bytes
- *@nmemb: number of element in the array
- *@size: bytes for each position in the array
- *Return: pointer void
+ *_memset - fill memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-
-	char *p;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
 
-	if (p == NULL)
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ * Return: pointer.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (!p)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
 	return (p);
-
 }
